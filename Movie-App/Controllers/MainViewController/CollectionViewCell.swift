bbfc8e431245644170fcd66167c7d34e027f8991
ebpearls <pranav.nepal@ebpearls.com>
2024-06-11 17:15:10 +0545

    nearly completed

---
+++ Movie-App/Controllers/MainViewController/CollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CollectionViewCell.swift
+//  Movie-App
+//
+//  Created by ebpearls on 6/11/24.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ Movie-App/Networking/APICaller.swift
@@ -0,0 +1,8 @@
+//
+//  APICaller.swift
+//  Movie-App
+//
+//  Created by ebpearls on 6/11/24.
+//
+
+import Foundation
